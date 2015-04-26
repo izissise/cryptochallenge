3ef700209cb29152a87cc9a858b5a2ae8c344ffe
Hugues Morisset <morisset.hugues@gmail.com>
2015-04-26 19:49:09 +0200

    commencing step 6

--- s1c6/s1c6.go
+++ s1c6/s1c6.go
@@ -3,7 +3,6 @@ package main
 import (
 	ch "characterFrequency"
 	b64 "encoding/base64"
-	"fmt"
 	"io/ioutil"
 )
 
@@ -20,63 +19,42 @@ func main() {
 	b64.StdEncoding.Decode(phrase64, []byte(dat))
 	phrase := string(phrase64)
 
-	var keysize int
-
-	for keysize = 2; keysize < 41; keysize++ {
+	hamdistMap := make(map[float32]int)
+	var hamdistSort []float32
+	for keysize := 2; keysize < 41; keysize++ {
 		block1 := phrase[0:keysize]
 		block2 := phrase[keysize:keysize]
 		hd := float32(ch.HammingDistanceStr(block1, block2)) / float32(keysize)
-		fmt.Println(hd, " -> ", keysize)
+		hamdistMap[hd] = keysize
+		hamdistSort = append(hamdistSort, hd)
 	}
-	/*
-		for i := 0; i < len(cyph); i++ {
-			res = res + string(cyph[i]^key[pos])
-			pos = (pos + 1) % len(key)
+
+	var keysizeList []int
+	for _, kk := range hamdistSort {
+		keysizeList = append(keysizeList, hamdistMap[kk])
+	}
+
+	for _, keySize := range keysizeList {
+		blockKeyTmp := make([]string, keySize)
+		for j := 0; j < keySize; j++ {
+			for i := 0; i < len(phrase); i += keySize {
+				blockKeyTmp[j] += string(phrase[i+j])
+			}
 		}
 
-		var key byte
 		allStrings := make([]string, 255)
-
 		for j := 0; j < 255; j++ {
-			key = byte(j)
+			key := byte(j)
 			var tmp string
-			for i := 0; i < len(cyph); i++ {
-				tmp += string(key ^ cyph[i])
+			for i := 0; i < len(blockKeyTmp); i++ {
+				tmp += string(key ^ blockKeyTmp[i])
 			}
 			allStrings[j] = tmp
 		}
 
-		var subString []string
-		for _, s := range allStrings {
-			print := true
-			var tmp string
-			for i := 0; i < len(s); i++ {
-				if !(strconv.IsPrint(int32(s[i]))) {
-					if print {
-						subString = append(subString, tmp)
-					}
-					print = false
-					tmp = ""
-				} else {
-					print = true
-					tmp = tmp + string(s[i])
-				}
-			}
-		}
+		for i := 0; i < len(allStrings); i++ {
 
-		freqMap := make(map[int]string)
-		for _, s := range subString {
-			perc := ch.CharacterFrequency(s)
-			freqMap[perc] = s
 		}
+	}
 
-		var keys []int
-		for k := range freqMap {
-			keys = append(keys, k)
-		}
-		sort.Ints(keys)
-
-		for _, k := range keys {
-			fmt.Println(k, " -> ", freqMap[k])
-		}*/
 }
